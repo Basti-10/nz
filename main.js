6bf2072c3de3858cbb6ac570ae52557f2cea0351
Bastian Wilbrandt <bastian.wilbrandt@student.uibk.ac.at>
2025-03-27 15:49:57 +0100

    3 variablen lat lng zoom definiert

--- main.js
+++ main.js
@@ -2,18 +2,22 @@
 Skript für die Neuseelandreise
 */
 
+let lng = 170.14;
+let zoom = 13;
+let lat = -43.59;
+
 // Karte initialisieren
-let map = L.map('map').setView([-43.59, 170.14], 13);
+let map = L.map('map').setView([lat, lng], zoom);
 
-        // Hintergrund definieren
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        }).addTo(map);
+// Hintergrund definieren
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
-        // Marker zeichnen
-        let marker = L.marker([-43.59, 170.14]).addTo(map);
+ // Marker zeichnen
+let marker = L.marker([lat, lng]).addTo(map);
 
-        // Popup definieren und öffnen
-        marker.bindPopup("<b>Mount Cook</b>").openPopup();
+ // Popup definieren und öffnen
+ marker.bindPopup("<b>Mount Cook</b>").openPopup();
 
