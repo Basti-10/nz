9940b5b3e1042e0f009b8cfb3b4d19df6a99bbba
Bastian Wilbrandt <bastian.wilbrandt@student.uibk.ac.at>
2025-03-27 15:36:20 +0100

    Kommentare ergänzt

--- main.js
+++ main.js
@@ -1,8 +1,16 @@
+/*
+Skript für die Neuseelandreise
+*/
+
+// Karte initialisieren
 var map = L.map('map').setView([-43.59, 170.14], 13);
+        // Hintergrund definieren
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         }).addTo(map);
+        // Marker zeichnen
         var marker = L.marker([-43.59, 170.14]).addTo(map);
+        // Popup definieren und öffnen
         marker.bindPopup("<b>Mount Cook</b>").openPopup();
 
